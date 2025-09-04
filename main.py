e4a9772ae8c8b3567ce33fc719f26be90c3f00fe
https-scpedro <8cansado@gmail.com>
2025-09-04 15:04:43 -0300

    refactor: remove features that will be moved to cogs

--- main.py
+++ main.py
@@ -1,45 +1,34 @@
+import discord as dc
+from discord.ext import commands
+import asyncio
 import os
 from dotenv import load_dotenv
-import discord as dc
-from discord.ext import commands, tasks
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = dc.Intents.all()
-bot = commands.Bot(command_prefix="!", intents = intents)
+intents = dc.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Bot inicializado!")                              # Printa no terminal
-    channel = await bot.fetch_channel(1111427627190861855)  # Busca o canal na API do Discord
-    await channel.send("Bot online!")                       # Envia mensagem no canal
+    print(f'Bot online')
+    print('------')
 
-@bot.event
-async def on_member_join(member:dc.Member):
-    embed = dc.Embed()
-    embed.title = "Bem-vindo(a)!"
-    embed.description = f"Olá {member.mention}, seja bem-vindo(a) ao servidor!"
-    channel = await bot.fetch_channel(1412958840214913145)
-    await channel.send(embed=embed)  
+async def load_cogs():
+    cogs_path = 'cogs'
+    for filename in os.listdir(cogs_path):
+        if filename.endswith('.py') and not filename.startswith('__'):
+            await bot.load_extension(f'{cogs_path}.{filename[:-3]}')
+            print(f'Cog "{filename[:-3]}" carregada!')
 
-@bot.command()
-async def ping(ctx:commands.Context):
-    await ctx.reply("Pong!")
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
 
-@bot.command()
-async def ola(ctx:commands.Context):
-    await ctx.reply("Olá, como vai?")
-
-@bot.command()
-async def profile(ctx:commands.Context):
-    pfp = ctx.author.avatar.url
-    embed = dc.Embed(title=f"Avatar de {ctx.author.display_name}")
-    embed.set_image(url=pfp)
-    await ctx.reply(embed=embed)
-
-@bot.command()
-async def teste(ctx):
-    await ctx.send("Funcionando!")
-
-bot.run(TOKEN)
+if __name__ == '__main__':
+    asyncio.run(main())
